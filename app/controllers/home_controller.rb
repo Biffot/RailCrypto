eaf9118d095959949537ef523eea797487f0e0c4
HarrisonCarmichael <harrisoncarmichael@outlook.com>
2018-03-24 16:29:35 +0000

    added new message to search ok yeah yesgit add .git add .!

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -7,10 +7,26 @@ class HomeController < ApplicationController
     @uri = URI(@url)
     @response = Net::HTTP.get(@uri)
     @coins = JSON.parse(@response)
-    @my_coins = ["BTC","XRP","ADA","XLM","STEEM","MIOTA","BCH","ETH",]
+    @my_coins = ["BTC","XRP","ADA","XLM","STEEM","MIOTA","BCH","ETH","USDT","NEO","XEM","TRX","ETC","BTG","BTCD","SRN",]
     @hello="hello me!"
     
   end
+  
   def about
+    
   end
+ 
+   def lookup
+   @symbol = params[:sym]
+   end
+   
+   if @symbol
+     @symbol = @symbol.upcase
+   end
+   
+   if @symbol == ""
+     @symbol =      "PS you forgot to write a crypto currency>!!!!!!!!!!"
+     
+     #ok hi
+   end
 end
