fe47bb9e5e4062e165e844ef569b7c2d97b92c6e
HarrisonCarmichael <harrisoncarmichael@outlook.com>
2018-03-25 12:29:05 +0000

    lookup coin price and ranks

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -17,6 +17,16 @@ class HomeController < ApplicationController
   end
  
    def lookup
+         require 'net/http'
+    require 'json'
+    
+    @url = 'https://api.coinmarketcap.com/v1/ticker/'
+    @uri = URI(@url)
+    @response = Net::HTTP.get(@uri)
+    @lookup_coins = JSON.parse(@response)
+  
+   
+     
    @symbol = params[:sym]
    end
    
